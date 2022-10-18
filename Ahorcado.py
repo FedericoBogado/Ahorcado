f55ee81cf4d0701a6b68f1ec0d9d507b868705ea
Fede <bogadofedericoezequiel@gmail.com>
2022-10-17 21:53:49 -0300

    Cree el archivo py del programa y el data.txt que contiene las palabras. Y defini las primeras funciones

---
+++ Ahorcado.py
@@ -0,0 +1,34 @@
+import random
+
+
+def read():
+    with open("./Data.txt", "r", encoding="UTF-8") as f:
+        pass
+
+
+def consultar_letra():
+    pass
+
+
+def animaciones():
+    pass
+
+
+def menu():
+    pass
+
+
+def palabra():
+    pass
+
+
+def vidas():
+    pass
+
+
+def run():
+    pass
+
+
+if __name__ == "__main__":
+    run()
