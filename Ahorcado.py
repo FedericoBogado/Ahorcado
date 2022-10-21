564b074322b97afde3578d0dc387587a4b24cd06
Fede <bogadofedericoezequiel@gmail.com>
2022-10-21 19:25:43 -0300

    Termine la mecanica principal y agregue un pequeo menu. Falta pulir con un conversor de acentos a normales

--- Ahorcado.py
+++ Ahorcado.py
@@ -12,39 +12,51 @@ def read():
     palabra_ram = palabra_ram.lower()
     palabra = list(palabra_ram.strip(" "))
     palabra.remove("\n")
-    return palabra
+    return (palabra) 
 
 
-def consultar_letra(palabra, vidas):
-    espacios = len(palabra)
+def mecanica_principal(palabra, palabra_list):
+    vidas = 3
+    espacios = len(palabra_list)
     palabra_jug = "_" * espacios
-    letra_jug = input("Ingrese una letra: ").lower()
-    i = 0
-    for letra in palabra:
-        i = i + 1
-        if letra_jug == letra:
-            palabra_jug = palabra_jug[:i-1] + letra + palabra_jug[i:]
-        else:
+    while vidas > 0:
+        letra_jug = input("\nIngrese una letra: ").lower()
+        i = 0
+        acerto = False
+        for letra in palabra_list:
+            i = i + 1
+            if letra_jug == letra:
+                palabra_jug = palabra_jug[:i-1] + letra + palabra_jug[i:]
+                acerto = True
+            else:
+                pass
+        if acerto == False:
             vidas = vidas - 1
-    print(palabra_jug)
-    return vidas
+        elif palabra == palabra_jug:
+            print("\nGanaste wachin\n")
+            exit()
+        else:
+            pass
+        print("\n" + palabra_jug)
 
 
 '''def animaciones():
-    pass
+    pass'''
 
 
-def menu():
-    pass
-'''
+def menu(palabra, palabra_list):
+    dec = input("1] Jugar\n2] Salir\n\n")
+    if dec == "1":
+        mecanica_principal(palabra, palabra_list)
+    elif dec == "2":
+        exit()
 
 
 def run():
-    vidas = 3
-    palabra = read()
-    while vidas > 0:
-        consultar_letra(palabra, vidas)
+    palabra_list = read()    
+    palabra = "".join(palabra_list)
     print(palabra)
+    menu(palabra, palabra_list)
 
 
 if __name__ == "__main__":
